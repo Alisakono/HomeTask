f165c48a68c130a8379ffad3cb9de5b5b59dc80f
Alisakono <alisa.konovalov@web.de>
2023-09-29 08:30:23 +0200

    Домашние задания.

---
+++ src/main/java/de/telran/ht260923.java
@@ -0,0 +1,31 @@
+package de.telran;
+
+public class ht260923 {
+    public static void main(String[] args) {
+        char myChar = 'G';
+        System.out.println('G');
+        int myInt = 89;
+        System.out.println(89);
+        byte myByte = 4;
+        System.out.println(4);
+        short myShort = 56;
+        System.out.println(56);
+        float myFloat = 4.7333436f;
+        System.out.println(4.7333436f);
+        double myDouble = 4.355453532;
+        System.out.println(4.3554535352);
+        long myLong = 12121;
+        System.out.print(12121);
+
+        /*2) В методе main инициализировать все примитивные типы.
+Вывести их результат в консоль:
+char: G
+int: 89
+byte: 4
+short: 56
+float: 4.7333436
+double: 4.355453532
+long: 12121*/
+
+    }
+}
