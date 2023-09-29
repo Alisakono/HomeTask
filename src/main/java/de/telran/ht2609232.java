9cebd033917b1447d83d4956e530cac9994fdee7
Alisakono <alisa.konovalov@web.de>
2023-09-29 08:49:36 +0200

    Домашние задания.

---
+++ src/main/java/de/telran/ht2609232.java
@@ -0,0 +1,24 @@
+package de.telran;
+
+public class ht2609232 {
+    public static void main(String[] args) {
+        int argF1 = 345, argF2 = 115;
+        int argF3 = 69, argF4 = 341;
+
+        int myIntF = argF1 / argF2;
+        int myInt = argF1 - argF4;
+        int myInt1 = argF1 / argF3;
+
+        System.out.println("argF1 / argF2 = "+myIntF);
+        System.out.println("argF1 - argF4 = "+myInt);
+        System.out.println("argF1 / argF3 = "+myInt1);
+
+
+
+        /*3) * Дано трехзначное число. Вывести на экран все цифры этого числа
+Пример: 345
+Вывод в консоль: Число 345 -> 3, 4, 5*/
+
+
+    }
+}
