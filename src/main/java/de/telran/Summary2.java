ea03ec6f73266b204d35710908f53d8e2f07196a
Alisakono <alisa.konovalov@web.de>
2023-10-08 15:59:43 +0200

    Д.з. от 28.09.23

--- src/main/java/de/telran/Summary2.java
+++ src/main/java/de/telran/Summary2.java
@@ -1,24 +1,24 @@
 package de.telran;
 
+import jdk.nashorn.internal.objects.NativeString;
+
 import java.util.Locale;
 
 public class Summary2 {
     public static void main(String[] args) {
-        System.out.println(getAbb("Deutsche", "Denokratische", "Republik"));
-        System.out.println(getAbb("Deutsche", "Denokratische", "Republik", "Freiheit"));
-
-    }
-
-    static String getAbb(String word1, String word2, String word3) {
-        return ("" + word1.charAt(0) +
-                word2.charAt(0) +
-                word3.charAt(0)).toUpperCase(Locale.ROOT);
-
+        System.out.println(getAbb("Deutsche","Denokratische","Republik","Freiheit"));
 
     }
 
     static String getAbb(String word1, String word2, String word3, String word4) {
-        return (getAbb(word1, word2, word3) + word4.charAt(0)).toUpperCase(Locale.ROOT);
+
+        return ("" + word1.charAt(0) +
+                word2.charAt(0) +
+                word3.charAt(0)+
+                word4.charAt(0)).toUpperCase(Locale.ROOT);
+/*
+    static String getAbb(String word1, String word2, String word3, String word4) {
+        return (getAbb(word1, word2, word3) + word4.charAt(0)).toUpperCase(Locale.ROOT);*/
 
     }
 }
