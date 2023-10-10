05df3b06439716d8e9b702ef7bc06dfd6c82ecc1
Alisakono <alisa.konovalov@web.de>
2023-10-10 16:18:09 +0200

    ht-05.10.23.2

---
+++ src/main/java/de/telran/Ht0510233.java
@@ -0,0 +1,20 @@
+package de.telran;
+
+
+import java.util.Scanner;
+
+public class Ht0510233 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму в евро: ");
+        int num = scanner.nextInt();
+        double sum = num * 1.02;
+        double i; i = 1.02;   // Курс евро к доллару
+        System.out.println("Сумма в долларах: "+sum);
+
+    }
+
+    }
+/*N3
+Программа запрашивает у пользователя сумму в Евро для конвертации
+Реализовать метод, который конвертирует полученную сумму в сумму в долларах США */
