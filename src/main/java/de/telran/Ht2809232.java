adccbc9a99068abd6ab7dfb1fa1b8d459edc0542
Alisakono <alisa.konovalov@web.de>
2023-10-10 17:41:55 +0200

    ht-02.10.23

--- src/main/java/de/telran/Ht2809232.java
+++ src/main/java/de/telran/Ht2809232.java
@@ -2,23 +2,15 @@ package de.telran;
 
 public class Ht2809232 {
     public static void main(String[] args) {
-
         int a = 250, b = 50;
-
         int sum = a + b;
         int mul = a * b;
-        int dif = a -b;
+        int dif = a - b;
         int dev = a / b;
 
         System.out.println(sum);
         System.out.println(mul);
         System.out.println(dif);
         System.out.println(dev);
-
-     /* № 2
-Создайте методы с математическими операциями +, -, *, /
-Каждый метод принимает два числа в параметрах и возвращает результат
-Вызовите все методы в main
-Результат распечатайте в консоль */
     }
 }
