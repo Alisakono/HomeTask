c82abfa2dd91283762d47e8c4045d9608b77476c
Alisakono <alisa.konovalov@web.de>
2023-10-21 20:18:14 +0200

    ht-9.10.23

---
+++ src/main/java/de/telran/HT910231.java
@@ -0,0 +1,17 @@
+package de.telran;
+public class HT910231 {
+    public static void main(String[] args) {
+        double m = 5.5;
+        double n = 5.45;
+        if (Math.abs(10 - m) > Math.abs(10 - n)) {
+                    System.out.println("Число n ближе к 10: " + n);
+                }
+                else {
+                    System.out.println("Число m  ближе к 10: " + m);
+                }
+            }
+        }
+
+
+
+
