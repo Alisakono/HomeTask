f8a4b5f5f90cc6d25a521457b3f3ff69a2332802
Alisakono <alisa.konovalov@web.de>
2023-10-21 17:55:36 +0200

    ht-15.10.23

---
+++ src/main/java/HT1510231.java
@@ -0,0 +1,20 @@
+public class HT1510231 {
+    public static void main(String[] args) {
+        int temp1 = 120;
+        int temp2 = 20;
+
+        if (temp1 > 100) System.out.println("Температура 1 "+true);
+            if (temp2 < 100) System.out.println("Температура 2 "+true); {
+            System.out.println("temp1 "+"temp2 "+true);
+        } System.out.println("temp1 "+"temp2 "+false);
+
+
+    }
+}
+        /* 1 уровень сложности: №1.
+         Представим, что у нас есть устройство, в котором две колбы.
+         Прибор работает корректно, если температура первой колбы выше 100 градусов,
+                 а температура второй колбы меньше 100 градусов.
+         Вы должны написать метод, который проверяет это устройство.
+         Ваша программа должна иметь переменные temperature1 и temperature2.
+         В результате метод возвращает true или false. */
